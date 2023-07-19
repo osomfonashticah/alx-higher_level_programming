00c64fe0d63f2b8b87b059e81d56244cfb58175c
Joseph Darko <osomfonashticah@gmail.com>
2023-07-19 23:35:59 +0000

    changes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,6 @@ CREATE USER
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost';
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
