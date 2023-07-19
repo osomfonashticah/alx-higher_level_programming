7ca4d725249628f9f867062441cc1e5f839c972a
Joseph Darko <osomfonashticah@gmail.com>
2023-07-19 23:52:42 +0000

    changes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- List privileges using SHOW GRANTS
--- DML QUERY to list all privileges for user_0d_1 and user_0d_2 in localhost
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
