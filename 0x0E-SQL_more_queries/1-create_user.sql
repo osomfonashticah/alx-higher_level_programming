64103a79e92b8982e2dec46ef1e7395774c5547c
Joseph Darko <osomfonashticah@gmail.com>
2023-07-19 23:42:22 +0000

    changes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,5 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Create an user with password
+-- DDL query to create a MySQL user with pasword
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- add privileges to user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
